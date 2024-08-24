bb276131c91f0b46bc2ef5b6127334e7faf1b505
Arnav Singh <arnav.singh3@ais.amity.edu>
2024-08-24 17:05:34 +0530

    Added metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,17 @@
 // This is the root layout component for your Next.js app.
 // Learn more: https://nextjs.org/docs/app/building-your-application/routing/pages-and-layouts#root-layout-required
+import { Metadata } from 'next';
 import { DM_Sans } from 'next/font/google'
 import { Space_Mono } from 'next/font/google'
 import { cn } from '@/lib/utils'
 import './globals.css'
 import { Analytics } from "@vercel/analytics/react"
+
+export const metadata: Metadata = {
+  title: 'New Salary Calculator',
+  description: 'New Salary Calculator quickly computes your updated salary after a percentage increase or the difference between old and new salaries.',
+  metadataBase: new URL('https://sal-cal.vercel.app/'),
+};
 
 const fontHeading = DM_Sans({
   subsets: ['latin'],
